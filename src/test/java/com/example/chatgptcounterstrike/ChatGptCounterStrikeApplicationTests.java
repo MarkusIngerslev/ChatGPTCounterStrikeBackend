085b4963841b89ed07959e910826e83af1dbf85e
MarkusIngerslev <markusolsen2004@gmail.com>
2024-04-16 15:49:47 +0200

    Initial commit

---
+++ src/test/java/com/example/chatgptcounterstrike/ChatGptCounterStrikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptcounterstrike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptCounterStrikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
